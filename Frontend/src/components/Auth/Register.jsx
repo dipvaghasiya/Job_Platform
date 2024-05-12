4e4ed0f101219063d9157e333380910d2f317a47
Smit Suriya <smitsuriya3203@gmail.com>
2024-05-12 23:20:42 +0530

    added functions in register components

--- Frontend/src/components/Auth/Register.jsx
+++ Frontend/src/components/Auth/Register.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../../main";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Navigate } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+
+  const { isAuthorized, setIsAuthorized, user, setUser } = useContext(Context);
+
+  const handleRegister = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.post(
+        "http://localhost:5000/api/v1/users/register",
+        { name, email, password, phone, role },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      toast.success(response.data.message);
+      setEmail("");
+      setName("");
+      setPassword("");
+      setRole("");
+      setPhone("");
+      setIsAuthorized(true);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthorized) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <>
+      <div className="authPage"></div>
+    </>
+  );
 };
 
 export default Register;
